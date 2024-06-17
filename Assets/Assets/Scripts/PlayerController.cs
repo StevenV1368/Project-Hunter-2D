d05e9820c9c43eb90121c46612c68889a36b8c83
AKolari <90071560+AKolari@users.noreply.github.com>
2024-06-17 00:06:49 -0400

    Added Player Movement and Groundcheck

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,64 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D playerBody;
+    private Animator playerAnimator;
+    private SpriteRenderer playerRenderer;
+
+
+    public float speed;
+    private float xMotion;
+    private bool isMoving = false;
+    private bool isAirborne = false;
+    public float jumpForce;
+
+
+
+
+    public Transform groundCheck;
+    public LayerMask groundLayers;
+    public float groundCheckRadius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerRenderer = GetComponent<SpriteRenderer>();
+        speed = 15f;
+        groundCheckRadius = .1f;
+        jumpForce = 25f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isAirborne = !Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayers);
+
+        xMotion = Input.GetAxis("Horizontal");
+        isMoving = xMotion != 0f;
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        this.transform.position += new Vector3(speed * xMotion * Time.deltaTime, 0f, 0f);
         
+
+
+
+        if (!isAirborne && Input.GetAxis("Jump")>0)
+        {
+            Debug.Log("Airborne");
+            playerBody.velocity += new Vector2(0f, jumpForce);
+        }
+
     }
 }
